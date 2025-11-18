028f21be0621d2497b8cd65e600126fd1a0253ff
Poliseno Riccardo <polisenoriccardo6@gmail.com>
2025-11-18 17:57:03 +0100

    progresso lezione 23

--- funcptr.c
+++ funcptr.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+void ciao() {
+  printf("Ciao\n");
+}
+
+/* void (*func)(void) 
+ * void = il tipo della funzione
+ * (*func) = il puntatore alla funzione
+ * (void) = gli argomenti accettati dalla funzione
+ *
+ * (*func) è semplificabile scrivendo solo func, il compilatore
+ * lo legge allo stesso modo, visto che i nomi delle funzioni sono puntatori
+ * a se stessi. Per chiarezza e leggibilità preferisco scrivere l'asterisco.
+  */
+void ripeti_n_volte(int n, void (*func)(void)) {
+  /* il while controlla se n è zero prima di decrementarlo 
+   * per poi decrementarlo */
+  while (n--)  
+    /* viene chiamata la funzione */
+    func(); 
+}
+
 int main(void) {
-  printf("%p\n", main); // I nomi delle funzioni sono puntatori
+  printf("main at %p\n", main); // I nomi delle funzioni sono puntatori
+  ripeti_n_volte(10, ciao);
   return 0;
 }
---
+++ funcptr2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+int main(void) {
+  int a[10];
+  for (int i = 0; i < 10; i++) 
+    a[j] = rand() & 15;
+  
+  return 0;
+}
